a4de14ee5d6b1200600fe9588ca3f50ba1da26d6
Spencer Snyder <sasnyde2@gmail.com>
2017-10-11 20:09:16 -0400

    removed unneeded js

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,11 +1,8 @@
 import '../sass/main.scss';
 import documentReady from './ready';
 import nav from './nav';
-import getList from './lister';
 
 // start javascript here
 documentReady(() => {
-  console.log('Document is ready to be operated on! \nNurse!,pass me the scalpal!');
-  nav(document.getElementById('openNav'));
-  getList();
+  nav(document.getElementById('openNav'), '30%');
 });
